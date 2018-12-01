40bbea0b4b3d3a4e699901e42c396933a847aaa4
karthikkharvi <karthik.kharvi@evry.com>
2018-12-01 13:15:10 +0530

    Implementation of Forms

--- Angular-Frontend/src/app/login/login.component.ts
+++ Angular-Frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginModel } from 'src/app/model/loginModel';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginModel:LoginModel ={} as LoginModel;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(){
+       
+      debugger;
+      var k = this.loginModel;
+  }
 }
